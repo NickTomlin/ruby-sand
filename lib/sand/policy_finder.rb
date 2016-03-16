687195ae78bed59888d22106f74e17b308bab915
Nick Tomlin <nick.tomlin@gmail.com>
2016-03-16 09:32:51 -0500

    feat: Make policy_finder compatible with AR/Sequel/Custom models

--- lib/sand/policy_finder.rb
+++ lib/sand/policy_finder.rb
@@ -20,14 +20,28 @@ module Sand
 
     private
 
+    # rubocop:disable Metrics/PerceivedComplexity, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/LineLength
     def find
       klass = @object
       # this is simplistic
       return klass if @object.nil?
 
-      klass = @object.to_s.camelize if @object.is_a?(Symbol)
-
-      "#{klass}#{POLICY_SUFFIX}"
+      if @object.respond_to?(:sand_policy)
+        @object.sand_policy
+      elsif @object.class.respond_to?(:sand_policy)
+        @object.sand_policy
+      else
+        klass = if @object.is_a?(Symbol)
+                  @object.to_s.camelize
+                elsif @object.respond_to?(:model)
+                  @object.model
+                elsif @object.respond_to?(:model_class)
+                  @object.model_class
+                else
+                  @object.to_s
+                end
+        "#{klass}#{POLICY_SUFFIX}"
+      end
     end
   end
 end
